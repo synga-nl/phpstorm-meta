60748c8d51dabd54e38e9384243ae6a348fd15c2
roy <info@roypouls.nl>
2016-02-05 18:28:50 +0100

    - Added ClassSelector

---
+++ src/Command/ClassSelector.php
@@ -0,0 +1,74 @@
+<?php
+namespace Synga\PhpStormMeta\Command;
+
+use Synga\InheritanceFinder\InheritanceFinderInterface;
+
+class ClassSelector extends IndependentCommandAbstract
+{
+    /**
+     * @var InheritanceFinderInterface
+     */
+    private $inheritanceFinder;
+
+    public function __construct(InheritanceFinderInterface $inheritanceFinderFactory) {
+        $this->inheritanceFinder = $inheritanceFinderFactory;
+    }
+
+    public function excludeCommand($applicationRoot) {
+        $classes = $this->getClasses($applicationRoot);
+
+        if (!empty($classes['included'])) {
+            $toBeExcluded = $this->output->choice('Which class do you want to exclude? You can give multiple values in a comma separated list', array_values($classes['included']), null, null, true);
+            $this->excludeClasses($applicationRoot, $classes['excluded'], $toBeExcluded);
+        } else {
+            $this->output->info('There are no classes to exclude');
+        }
+    }
+
+    public function includeCommand($applicationRoot) {
+        $classes = $this->getClasses($applicationRoot);
+
+        if (!empty($classes['excluded'])) {
+            $toBeExcluded = $this->output->choice('Which class do you want to include? You can give multiple values in a comma separated list', array_values($classes['excluded']), null, null, true);
+            $this->includeClasses($applicationRoot, $classes['excluded'], $toBeExcluded);
+        } else {
+            $this->output->info('There are no classes to include');
+        }
+    }
+
+    public function excludeClasses($applicationRoot, $classes, $toBeExcluded) {
+        $file = $applicationRoot . '/.phpstorm.meta.cache';
+        file_put_contents($file, serialize(array_unique(array_merge($classes, $toBeExcluded))));
+    }
+
+    protected function includeClasses($applicationRoot, $classes, $toBeIncluded) {
+        $file = $applicationRoot . '/.phpstorm.meta.cache';
+        var_dump(array_diff($classes, $toBeIncluded));
+        file_put_contents($file, serialize(array_unique(array_diff($classes, $toBeIncluded))));
+    }
+
+    protected function getClasses($applicationRoot) {
+        $result = ['excluded' => [], 'included' => [], 'all' => []];
+
+        $classes = $this->inheritanceFinder->findImplements('Synga\PhpStormMeta\PhpStormMetaExtensionInterface');
+        if (is_array($classes)) {
+            foreach ($classes as $class) {
+                $result['all'][] = $class->getFullQualifiedNamespace();
+            }
+        }
+
+        $cachePath = $applicationRoot . '/.phpstorm.meta.cache';
+        if (file_exists($cachePath)) {
+            $result['excluded'] = unserialize(file_get_contents($cachePath));
+        }
+
+        $result['included'] = array_diff($result['all'], $result['excluded']);
+        var_dump($result);
+
+        return $result;
+    }
+
+    protected function writeToCacheFile() {
+
+    }
+}
